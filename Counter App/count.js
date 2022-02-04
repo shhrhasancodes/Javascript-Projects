61a95c0131ff3e801ceaa42e72b0e338426ae844
shhrhasan <shhrhasan.com>
2022-02-04 19:02:59 +0500

    counter file added

---
+++ Counter App/count.js
@@ -0,0 +1,18 @@
+let btn = document.querySelector(".button2"),
+ rst = document.querySelector(".button3"),
+content = document.querySelector(".button1");
+
+let score = 0;
+
+content.textContent = score;
+
+btn.addEventListener("click" , ()=>{
+    score ++;
+    content.textContent = score;
+});
+
+rst.addEventListener("click" , ()=>{
+    score = 0;
+    content.textContent = score;
+});
+
