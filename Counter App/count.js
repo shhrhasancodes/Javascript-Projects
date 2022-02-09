947ca19aa25bdefb761cde8edb22ce1961a72005
shhrhasan <shhrhasan.com>
2022-02-09 23:11:36 +0500

    files renamed

--- Counter App/count.js
+++ Counter App/count.js
@@ -1,18 +1,17 @@
 let btn = document.querySelector(".button2"),
- rst = document.querySelector(".button3"),
-content = document.querySelector(".button1");
+    rst = document.querySelector(".button3"),
+    content = document.querySelector(".button1");
 
 let score = 0;
 
 content.textContent = score;
 
-btn.addEventListener("click" , ()=>{
-    score ++;
+btn.addEventListener("click", () => {
+    score++;
     content.textContent = score;
 });
 
-rst.addEventListener("click" , ()=>{
+rst.addEventListener("click", () => {
     score = 0;
     content.textContent = score;
 });
-
